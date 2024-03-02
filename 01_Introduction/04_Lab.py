ce87b41e40208fe5768afb0b99222cf5cd1599a2
mertnamsal <namsalmert@gmail.com>
2024-03-02 20:20:28 +0300

    02.03.2024

---
+++ 01_Introduction/04_Lab.py
@@ -0,0 +1,20 @@
+
+#While loop
+
+#Exp 1 :
+#1 ile 100 arası ekrana yazdır
+# sayac=0
+# while sayac <= 100:
+#     print(sayac)
+#     sayac += 1
+
+#Exp 2:
+#1 ile 100 arasındaki sayıları toplayarak toplamı yazdır
+
+# sayac = 1
+# toplam = 0
+# while sayac <=100:
+#     toplam +=sayac
+#     sayac +=1
+# print(toplam)
+
---
+++ 01_Introduction/05_Lab.py
@@ -0,0 +1,11 @@
+# in   & not in
+
+name ="mert"
+print('m' in name)
+
+#range()
+
+for i in range(10):
+    print(i,end =",")
+
+#range(10 , 101 , 5)  -> 10 dan başlayıp 101 e kadar 5er 5er arrtacak bir tam sayı listesi oluşturur.
