0dbaf312a24fda11525132aa0b232639afff4029
mertnamsal <namsalmert@gmail.com>
2024-03-03 20:23:50 +0300

    03.03.2024

--- 01_Introduction/05_Lab.py
+++ 01_Introduction/05_Lab.py
@@ -1,11 +1,34 @@
 # in   & not in
 
-name ="mert"
-print('m' in name)
+# name ="mert"
+# print('m' in name)
 
 #range()
 
-for i in range(10):
-    print(i,end =",")
+# for i in range(10):
+#     print(i,end =",")
 
 #range(10 , 101 , 5)  -> 10 dan başlayıp 101 e kadar 5er 5er arrtacak bir tam sayı listesi oluşturur.
+
+
+# Exp
+# 0 - 100 arasında ki çift ve tek sayıların toplamlarını ayrı ayrı ekrana yazdıralım
+
+# cift = 0
+# tek = 0
+# for i in range(101):
+#     if i % 2 == 0:
+#         cift += i
+#     else:
+#         tek += i
+# print(f'tekler : {tek}  \n çiftler : {cift}')
+
+#Nested For Loop
+# for i in range(10):
+#     for j in range(10):
+#         pass
+
+for i in range(4,0,-1):
+    for j in range (i-1,4):
+        print("x", end="")
+    print(' ')
---
+++ 01_Introduction/06_Lab.py
@@ -0,0 +1,51 @@
+# List
+#index mantığıyla çalışır
+#Uygulama içerisinde anlık bizim bir yada birden fazla değer tutan yapılardır.Farklı tiplerde değerler tutabilirler.Listelerde Ram üzerinde ..... alanında tutulduğu için değişkenler gibi uygulama sonlandığında RAM'den uçurulurlar yani Run Time içerisinde eklenilen değerleri Hard Disk gibi saklayamazlar.
+from random import randint
+
+#Listeler index mantığı ile çalışırlar.Liste içerisinde saklanılan değerler sıfırıncı
+
+
+
+# word = input("PLease type").lower()
+# for c in word:
+#     print(c)
+#
+# liste = ['a', 'e', 'i', 'ı', 'o', 'ö', 'u', 'ü']
+# sesli_harfler = []
+# sessiz_harfler = []
+#
+# for c in word:
+#     if c.isalpha():
+#         if c not in sesli_harfler and c not in sessiz_harfler:
+#             if c in liste:
+#                 sesli_harfler.append(c)
+#             else:
+#                 sessiz_harfler.append(c)
+# print(sessiz_harfler)
+# print(sesli_harfler)
+
+#Exp  iki listeyi random sayılar ile dolduralalım
+#akabinde benzer index lerde tutulan değeleri toplayarak 3. listede gene aynı index'se yazalm
+random_list1 = []
+random_list2 = []
+random_list3 = []
+for i in range(10):
+    random_list1.insert(i,randint(0,100))
+    random_list2.insert(i,randint(0,100))
+    random_list3.insert(i,(random_list1[i]+random_list2[i]))
+
+print(random_list1)
+print(random_list2)
+print(random_list3)
+#users listesi içerisinde bulunan kullanıcılara kurumsal mail adresi oluşturalım
+users  = ['burak yilmaz', 'mert namsaal','kerim abdul cabbar okkes',' ']
+mail_adress = []
+
+
+#kullanıcının girdiği password is valid mi ?
+#1. girilen şifre en az 16 karakter uzunlugunda olmalı
+# en az bir büyük bir kücük
+# en az bir rakam
+# en az bir noktalama işareti
+# herhangi bir ifade tekrar etmemeli
