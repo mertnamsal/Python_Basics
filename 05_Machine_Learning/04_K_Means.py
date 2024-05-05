069f033e5523a1a2a7b3323a2f8391f09c0384c3
mertnamsal <namsalmert@gmail.com>
2024-05-05 18:27:01 +0300

    05.05.2024

--- 05_Machine_Learning/04_K_Means.py
+++ 05_Machine_Learning/04_K_Means.py
@@ -1,5 +1,7 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 
@@ -31,8 +33,21 @@ Clus_dataSet = StandardScaler().fit_transform(X)
 k_means = KMeans(init='k-means++', n_clusters=3, n_init=12)
 k_means.fit(X)
 labels = k_means.labels_
-print(labels)
+# print(labels)
 
 df['Clus_km'] = labels
-print(df.head(20).to_string())
+# print(df.head(20).to_string())
+# Küme merkez noktalarının kordinatları
+# print(k_means.cluster_centers_)
+
+fig = plt.figure(1,figsize=(10,7))
+plt.clf()
+# ax = Axes3D(fig, rect=[0,0,0.95,1], elev=48, azim=134)
+ax = fig.add_subplot(111,projection='3d')
+plt.cla()
+ax.scatter(X[:,1],X[:,0],X[:,3], c=labels.astype(np.float_))
+ax.set_xlabel('Education')
+ax.set_ylabel('Age')
+ax.set_zlabel('Income')
+
 
